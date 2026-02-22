ed0093853448c23af5eb949200c72987a0e8ede2
yaara <yaara3332@gmail.com>
2026-02-23 00:35:15 +0200

    commit2

--- app/src/main/java/com/example/demo_10/MainActivity.java
+++ app/src/main/java/com/example/demo_10/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.demo_10;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText display;
+    String opernd = "";
+    double number = 0.0;
+    String txt;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        display = findViewById(R.id.buttonEquals);
     }
+
+    private void calculate() {
+        txt = display.getText().toString();
+
+        if (txt.isEmpty())
+        {
+            return;
+        }
+
+        if (opernd.equals("plus"))
+        {
+            number = number + Double.parseDouble(txt);
+        }
+        else if (opernd.equals("minus"))
+        {
+            number = number - Double.parseDouble(txt);
+        }
+        else if (opernd.equals("mult"))
+        {
+            number = number * Double.parseDouble(txt);
+        }
+        else if (opernd.equals("div"))
+        {
+            if (Double.parseDouble(txt) != 0.0)
+            {
+                number = number / Double.parseDouble(txt);
+            }
+        }
+
+        else
+        {
+            number = Double.parseDouble(txt);
+        }
+
+
+
+    }
+
+
 }
+
